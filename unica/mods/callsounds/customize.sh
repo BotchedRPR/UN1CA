7424ce00dd5c64b5ccdece8018d5b465a97a75df
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-08 16:13:35 +0200

    unica: mods: delete a lot of mods
    * Will add some of them back in the future
    
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/mods/callsounds/customize.sh
+++
@@ -1,4 +0,0 @@
-if [ ! -d "$APKTOOL_DIR/system/priv-app/Telecom/Telecom.apk" ]; then
-    bash "$SRC_DIR/scripts/apktool.sh" d "/system/priv-app/Telecom/Telecom.apk"
-fi
-cp -a --preserve=all "$SRC_DIR/unica/mods/callsounds/Telecom.apk/"* "$APKTOOL_DIR/system/priv-app/Telecom/Telecom.apk"
