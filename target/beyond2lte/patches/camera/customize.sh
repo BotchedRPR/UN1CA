ed8cda30fc9784b96475beaef115ada842527aec
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-10 14:53:03 +0200

    target: beyond2lte: add missing SNAP NDK v2 lib
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- target/beyond2lte/patches/camera/customize.sh
+++ target/beyond2lte/patches/camera/customize.sh
@@ -125,3 +125,17 @@ for blob in $BLOBS_LIST
 do
     ADD_TO_WORK_DIR "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0"
 done
+
+if ! grep -q "libeden_wrapper_system" "$WORK_DIR/configs/file_context-system"; then
+    {
+        echo "/system/lib64/libeden_wrapper_system\.so u:object_r:system_lib_file:s0"
+        echo "/system/lib64/vendor\.samsung\.hardware\.snap-V2-ndk\.so u:object_r:system_lib_file:s0"
+
+    } >> "$WORK_DIR/configs/file_context-system"
+fi
+if ! grep -q "libeden_wrapper_system" "$WORK_DIR/configs/fs_config-system"; then
+    {
+        echo "system/lib64/libeden_wrapper_system.so 0 0 644 capabilities=0x0"
+        echo "system/lib64/vendor.samsung.hardware.snap-V2-ndk.so 0 0 644 capabilities=0x0"
+    } >> "$WORK_DIR/configs/fs_config-system"
+fi
