7336d4d4ef73223c89e4d7d1f0d478eaecaa828c
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-10 14:04:57 +0200

    target: beyond2lte: update CameraX debloat
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- target/beyond2lte/debloat.sh
+++ target/beyond2lte/debloat.sh
@@ -29,9 +29,13 @@ system/etc/permissions/cameraservice.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.globalpostprocmgr.xml
 system/etc/permissions/privapp-permissions-com.samsung.petservice.xml
 system/etc/permissions/privapp-permissions-com.samsung.videoscan.xml
+system/etc/permissions/sec_camerax_impl.xml
+system/etc/permissions/sec_camerax_service.xml
+system/framework/sec_camerax_impl.jar
 system/framework/scamera_sep.jar
 system/priv-app/GlobalPostProcMgr
 system/priv-app/PetService
 system/priv-app/SCameraSDKService
+system/priv-app/sec_camerax_service
 system/priv-app/VideoScan
 "
