9bc44c2f675e3e516e8ede4828a3254d7e6ca236
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-03-10 14:53:54 +0200

    unica: patches: desixtification: fix typo
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- unica/patches/desixtification/customize.sh
+++ unica/patches/desixtification/customize.sh
@@ -154,7 +154,7 @@ if [ -f "$FW_DIR/${MODEL}_${REGION}/vendor/lib/libdrm.so" ]; then
 
     # Downgrade libengmode
     ADD_TO_WORK_DIR "system" "system/lib64/lib.engmode.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "system" "system/lib64/lib.engmode.samsung_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "system" "system/lib64/lib.engmodejni.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
     # Add missing camera blobs
     ADD_TO_WORK_DIR "system" "system/lib64/libtensorflowLite.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
